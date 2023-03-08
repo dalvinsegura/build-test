3c5054b784394a41059b1aec12ead75902de3d29
Dalvin Segura <byjacson18@gmail.com>
2023-03-08 19:25:24 -0400

    allowedOriginsUpdated

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 export const allowedOrigins = [
-    'http://localhost:5173',
-    'http://localhost:5965',
-    "http://127.0.0.1:5173"
+  "http://localhost:5173",
+  "http://localhost:5965",
+  "http://127.0.0.1:5173",
+  "https://symphonious-beijinho-465648.netlify.app",
 ];
 
-export default allowedOrigins
+export default allowedOrigins;
